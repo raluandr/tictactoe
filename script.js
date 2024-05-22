39447a8825ae2e2bc915ec1771b8954294ad4897
Ralu <102420780+raluandr@users.noreply.github.com>
2024-05-22 23:16:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let gameActive = true;
 let currentPlayer = 'X';
 let gameState = ['', '', '', '', '', '', '', '', ''];
 
-// Winning conditions
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -20,37 +19,35 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
-// Function to handle cell click
+
 function handleCellClick(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
 
-    // If cell is already occupied or game is not active, do nothing
+    
     if (gameState[clickedCellIndex] !== '' || !gameActive) {
         return;
     }
 
-    // Update game state and cell UI
     gameState[clickedCellIndex] = currentPlayer;
     clickedCell.textContent = currentPlayer;
 
-    
-    // Switch player
+   
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     statusDisplay.textContent = `It's ${currentPlayer}'s turn`;
-    // Check for win
+    
     checkWin();
 
 }
 
-// Function to check for win
+
 function checkWin() {
     console.log('Checking for win...');
 
     for (let i = 0; i < winningConditions.length; i++) {
         const [a, b, c] = winningConditions[i];
         if (gameState[a] !== '' && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
-            console.log(`${currentPlayer} wins!`); // Log winning message
+            console.log(`${currentPlayer} wins!`); 
             gameActive = false;
             statusDisplay.textContent = `WINNER!`;
 
@@ -62,7 +59,7 @@ function checkWin() {
         }
     }
 
-    // Check for draw
+    
     let draw = !gameState.includes('');
     if (draw) {
         console.log("It's a draw!"); // Log draw message
@@ -72,25 +69,24 @@ function checkWin() {
     }
 }
 
-// Call the checkWin function after setting up the game
+
 checkWin();
 
 
 
-// Function to handle restart game
+
 function restartGame() {
     gameActive = true;
     currentPlayer = 'X';
     gameState = ['', '', '', '', '', '', '', '', ''];
     statusDisplay.textContent = `It's ${currentPlayer}'s turn`;
 
-    // Clear cell content
     cells.forEach(cell => {
         cell.textContent = '';
         cell.classList.remove('win');
     });
 }
 
-// Add event listeners
+
 cells.forEach(cell => cell.addEventListener('click', handleCellClick));
 restartButton.addEventListener('click', restartGame);
